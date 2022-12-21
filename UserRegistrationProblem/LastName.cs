e640fb4b6b6339d0a5157d5e5fcb36f0fb9fb007
shubham07h <shubham1609sharma@gmail.com>
2022-12-21 23:54:43 +0530

    commit

---
+++ UserRegistrationProblem/LastName.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    internal class LastName
+    {
+        public void Last()
+        {
+            Console.WriteLine("Enter user last name");
+            var data = Console.ReadLine();
+            string pattern = "^[A-Z]{1}[a-z]{2,}";
+            Regex regex = new Regex(pattern);
+            if (regex.IsMatch(data))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(data + " is valid");
+                Console.ResetColor();
+
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(data + " is invalid");
+                Console.ResetColor();
+
+            }
+        }
+    }
+}
