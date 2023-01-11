9d83049e572ca23a3acbab916246351d9f5ce044
shubham07h <shubham1609sharma@gmail.com>
2023-01-11 16:20:56 +0530

    commit

--- UserRegistrationProblem/Program.cs
+++ UserRegistrationProblem/Program.cs
@@ -1,19 +1,41 @@
-﻿namespace UserRegistration
+﻿using System;
+using System.Text.RegularExpressions;//namespace for regex
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace UserRegistration
 {
     public class Program
     {
         static void Main(string[] args)
-
         {
+            ValidUserInfo re = new ValidUserInfo();
 
-            RuleOne min = new RuleOne();
-            min.Min8();
+            Console.WriteLine("Enter first Name");
+            string first = Convert.ToString(Console.ReadLine());
 
+            Console.WriteLine("Enter Last Name");
+            string last = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Enter Valid Email ");
+            string mail = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Enter Valid Phone number ");
+            string phone = Convert.ToString(Console.ReadLine());
+
+            // Console.WriteLine("Enter Valid Password with minimum 8 characters ");
+            Console.WriteLine("Enter Valid Password with minimum 8 characters with Atleast 1 Upper Case ");
+            string pass1 = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine(re.validateFirstName(first));
+            Console.WriteLine(re.validateLastName(last));
+            Console.WriteLine(re.validateEmail(mail));
+            Console.WriteLine(re.validatePhone(phone));
+            Console.WriteLine(re.validatePass1(pass1));
         }
-
     }
-
 }
-
-
-
---
+++ UserRegistrationProblem/ValidUserInfo.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace UserRegistration
+{
+    internal class ValidUserInfo
+    {
+        public static string FIRSTNAME = "^[A-Z]{1}[a-z]{2,}";
+        public static string LASTNAME = "^[A-Z]{1}[a-z]{2,}";
+        public static string EMAIL = "^[0-9A-Za-z]+([.][0-9A-Za-z]+)[@][0-9A-Za-z]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
+        public static string PHONE = "^[+][0-9]{1,3}[ ][6-9]{1}[0-9]{9}$";
+        public static string PASSWORD1 = "^(?=.*?[A-Z]).{8,}$";
+        //Rule1-min 8 character "^[A-z]{8,}$"       
+
+        public bool validateFirstName(string fname)
+        {
+            return Regex.IsMatch(fname, FIRSTNAME);
+        }
+        public bool validateLastName(string lname)
+        {
+            return Regex.IsMatch(lname, LASTNAME);
+        }
+        public bool validateEmail(string mail)
+        {
+            return Regex.IsMatch(mail, EMAIL);
+        }
+        public bool validatePhone(string phone)
+        {
+            return Regex.IsMatch(phone, PHONE);
+        }
+        public bool validatePass1(string pass1)
+        {
+            return Regex.IsMatch(pass1, PASSWORD1);
+        }
+    }
+}
